27ee8e92421bdc912736b2f70185bdb19c156200
saad naseer <saadnaseer909@gmail.com>
2023-06-16 17:01:34 +0500

    resolve type error in page.tsx

--- types/index.ts
+++ types/index.ts
@@ -37,6 +37,9 @@ export interface FilterProps {
   limit: number;
   model: string;
 }
+export interface HomeProps {
+  searchParams: FilterProps;
+}
 
 export interface OptionProps{
   title:string;
